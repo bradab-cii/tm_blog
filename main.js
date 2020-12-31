1f96b71e52751317b0714270b0b419d9fe91479a
Bradab <sokeasang69@gmail.com>
2020-12-31 18:39:29 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const searchIcon = document.querySelector(".search-icon");
+const searchContainer = document.querySelector(".search-container");
+const mobileMenuContainer =document.querySelector(".mobile-menu-container");
+const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+const mobileMenuCloseIcon = document.querySelector(".menu-close-icon");
+
+searchIcon.addEventListener("click",function(){
+    searchContainer.classList.toggle("active");
+})
+mobileMenuIcon.addEventListener("click",function(){
+    mobileMenuContainer.classList.add("active");
+})
+mobileMenuCloseIcon.addEventListener("click",function(){
+    mobileMenuContainer.classList.remove("active");
+})  
